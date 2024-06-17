0d9181c130e6ef08eb04b7690fefd9582cde187a
Aleksandr Repin <admin@appmon.ru>
2024-06-17 12:03:06 +0300

    fix: actions list generator added to Options controller

--- src/controllers/root/ncOptions.js
+++ src/controllers/root/ncOptions.js
@@ -1,176 +1,166 @@
-import Validators from '../common/validators.js';
-import {
-	Frame
-} from 'not-bulma';
+import Validators from "../common/validators.js";
+import { Frame } from "not-bulma";
 
 const notCRUD = Frame.notCRUD;
 
-import UIImport from '../common/import.svelte';
+import UIImport from "../common/import.svelte";
+import CRUDActionList from "not-bulma/src/frame/crud/actions/list";
 
 const LABELS = {
-	plural: 'Настройки',
-	single: 'Настройка',
+    plural: "Настройки",
+    single: "Настройка",
 };
 
-const MODEL = 'options';
+const MODEL = "options";
 
 class ncOptions extends notCRUD {
-	static MODEL_NAME = MODEL;
-	static MODULE_NAME = '';
-	constructor(app, params, schemes) {
-		super(app, MODEL);
-		this.setModuleName('');
-		this.setModelName(MODEL);
-		this.setOptions('names', LABELS);
-		this.setOptions('Validators', Validators);
-		this.setOptions('params', params);
-		this.setOptions('role', 'root');
-		this.setOptions('urlSchemes', schemes);
-		this.setOptions('list', {
-			actions: [{
-					title: 'Экспорт',
-					action: this.openExport.bind(this)
-				},
-				{
-					title: 'Импорт',
-					action: this.openImport.bind(this)
-				}
-			],
-			fields: [{
-				path: ':optionsID',
-				title: 'ID',
-				searchable: true,
-				sortable: true
-			}, {
-				path: ':id',
-				title: 'Название',
-				searchable: true,
-				sortable: true
-			}, {
-				path: ':value',
-				title: 'Значение',
-				searchable: true,
-				sortable: true,
-				hideOnMobile: true
-			}, {
-				path: ':active',
-				title: 'Активна',
-				type: 'boolean',
-				searchable: true,
-				sortable: true,
-				preprocessor: (value) => {
-					return [{
-						value
-					}];
-				},
-				hideOnMobile: true
-			}, {
-				path: ':_id',
-				title: 'Действия',
-				type: 'button',
-				preprocessor: (value) => {
-					return [{
-							action: this.goDetails.bind(this, value),
-							title: 'Подробнее',
-							size: 'small'
-						},
-						{
-							action: this.goUpdate.bind(this, value),
-							title: 'Изменить',
-							size: 'small'
-						},
-						{
-							action: this.goDelete.bind(this, value),
-							type: 'danger',
-							title: 'Удалить',
-							size: 'small',
-							style: 'outlined'
-						}
-					];
-				},
-			}]
-		});
-		this.start();
-		return this;
-	}
+    static MODEL_NAME = MODEL;
+    static MODULE_NAME = "";
+    constructor(app, params, schemes) {
+        super(app, MODEL);
+        this.setModuleName("");
+        this.setModelName(MODEL);
+        this.setOptions("names", LABELS);
+        this.setOptions("Validators", Validators);
+        this.setOptions("params", params);
+        this.setOptions("role", "root");
+        this.setOptions("urlSchemes", schemes);
+        this.setOptions("list", {
+            actions: [
+                {
+                    title: "Экспорт",
+                    action: this.openExport.bind(this),
+                },
+                {
+                    title: "Импорт",
+                    action: this.openImport.bind(this),
+                },
+            ],
+            fields: [
+                {
+                    path: ":optionsID",
+                    title: "ID",
+                    searchable: true,
+                    sortable: true,
+                },
+                {
+                    path: ":id",
+                    title: "Название",
+                    searchable: true,
+                    sortable: true,
+                },
+                {
+                    path: ":value",
+                    title: "Значение",
+                    searchable: true,
+                    sortable: true,
+                    hideOnMobile: true,
+                },
+                {
+                    path: ":active",
+                    title: "Активна",
+                    type: "boolean",
+                    searchable: true,
+                    sortable: true,
+                    preprocessor: (value) => {
+                        return [
+                            {
+                                value,
+                            },
+                        ];
+                    },
+                    hideOnMobile: true,
+                },
+                {
+                    path: ":_id",
+                    title: "Действия",
+                    type: "button",
+                    preprocessor: (value) => {
+                        return CRUDActionList.createActionsButtons(this, value);
+                    },
+                },
+            ],
+        });
+        this.start();
+        return this;
+    }
 
-	createDefault() {
-		let newRecord = this.make[this.getModelName()]({
-			'_id': null,
-			id: LABELS.single,
-			value: '',
-			active: true,
-		});
-		return newRecord;
-	}
+    createDefault() {
+        let newRecord = this.make[this.getModelName()]({
+            _id: null,
+            id: LABELS.single,
+            value: "",
+            active: true,
+        });
+        return newRecord;
+    }
 
-	openExport() {
-		window.location.assign('/api/options/export');
-		/*this.make[this.getModelName()]({}).$export()
+    openExport() {
+        window.location.assign("/api/options/export");
+        /*this.make[this.getModelName()]({}).$export()
 			.then((data)=>{
 				this.log(data);
 			})
 			.catch(e => this.error(e));*/
-	}
+    }
 
-	closeImportPopup() {
-		if (this.ui.import) {
-			this.ui.import.$destroy();
-			this.ui.import = null;
-		}
-	}
+    closeImportPopup() {
+        if (this.ui.import) {
+            this.ui.import.$destroy();
+            this.ui.import = null;
+        }
+    }
 
-	openImport() {
-		this.log('import');
-		this.closeImportPopup();
-		this.ui.import = new UIImport({
-			target: document.body
-		});
-		this.ui.import.$on('reject', (ev) => {
-			if (ev.detail.error) {
-				this.error(ev.detail.error);
-			}
-			this.closeImportPopup();
-		});
-		this.ui.import.$on('resolve', () => this.closeImportPopup());
-		this.ui.import.$on('import', async (ev) => {
-			if (ev.detail.options) {
-				try {
-					let res = await this.requestImport(ev.detail.options);
-					this.log(res);
-					if (res.status === 'ok') {
-						if (res.result && res.result.info) {
-							this.ui.import.setInfo(res.result.info);
-							this.afterImportSuccess(res.result);
-						}
-					} else {
-						this.error(res.error);
-						this.ui.import.setError(res.error);
-					}
-				} catch (e) {
-					this.ui.import.setError(e.message);
-					this.error(e);
-				}
-			}
-		});
-	}
+    openImport() {
+        this.log("import");
+        this.closeImportPopup();
+        this.ui.import = new UIImport({
+            target: document.body,
+        });
+        this.ui.import.$on("reject", (ev) => {
+            if (ev.detail.error) {
+                this.error(ev.detail.error);
+            }
+            this.closeImportPopup();
+        });
+        this.ui.import.$on("resolve", () => this.closeImportPopup());
+        this.ui.import.$on("import", async (ev) => {
+            if (ev.detail.options) {
+                try {
+                    let res = await this.requestImport(ev.detail.options);
+                    this.log(res);
+                    if (res.status === "ok") {
+                        if (res.result && res.result.info) {
+                            this.ui.import.setInfo(res.result.info);
+                            this.afterImportSuccess(res.result);
+                        }
+                    } else {
+                        this.error(res.error);
+                        this.ui.import.setError(res.error);
+                    }
+                } catch (e) {
+                    this.ui.import.setError(e.message);
+                    this.error(e);
+                }
+            }
+        });
+    }
 
-	afterImportSuccess(result) {
-		let delay = parseInt(result.shutdown) + 2000;
-		this.refresh(delay);
-		setTimeout(() => this.closeImportPopup(), delay);
-	}
+    afterImportSuccess(result) {
+        let delay = parseInt(result.shutdown) + 2000;
+        this.refresh(delay);
+        setTimeout(() => this.closeImportPopup(), delay);
+    }
 
-	requestImport(data) {
-		return this.make[this.getModelName()]({
-			options: data
-		}).$import();
-	}
+    requestImport(data) {
+        return this.make[this.getModelName()]({
+            options: data,
+        }).$import();
+    }
 
-	getItemTitle(item) {
-		return `${item.optionsID}#${item.id}`;
-	}
-
+    getItemTitle(item) {
+        return `${item.optionsID}#${item.id}`;
+    }
 }
 
 export default ncOptions;
--- src/routes/options.js
+++ src/routes/options.js
@@ -1,336 +1,398 @@
-const
-	UserActions = [],
-	AdminActions = [
-		'create',
-		'getRaw',
-		'list',
-		'listAndCount',
-		'listAll'
-	],
-	MODEL_NAME = 'Options',
-	MODEL_OPTIONS = {
-		MODEL_NAME,
-		MODEL_TITLE: 'Опции',
-		populate: {},
-		RESPONSE: {
-			full: ['get', 'getRaw', 'create']
-		},
-	};
+const UserActions = [],
+    AdminActions = ["create", "getRaw", "list", "listAndCount", "listAll"],
+    MODEL_NAME = "Options",
+    MODEL_OPTIONS = {
+        MODEL_NAME,
+        MODEL_TITLE: "Опции",
+        populate: {},
+        RESPONSE: {
+            full: ["get", "getRaw", "create"],
+        },
+    };
 
-const log = require('not-log')(module, `${MODEL_NAME} router`);
+const log = require("not-log")(module, `${MODEL_NAME} router`);
 
-const notNode = require('not-node');
-const notError = require('not-error').notError;
-const notLocale = require('not-locale');
-const query = require('not-filter');
-const metaExtend = require('not-meta').extend;
-const metaRoute = require('not-meta').Route;
-const getIP = notNode.Auth.getIP;
+const notNode = require("not-node");
+const notError = require("not-error").notError;
+const notLocale = require("not-locale");
+const query = require("not-filter");
+const metaExtend = require("not-meta").extend;
+const metaRoute = require("not-meta").Route;
+const getIP = notNode.Common.getIP;
 
 exports.before = (req) => {
-	let optionsSchema = this.getModelSchema(MODEL_NAME);
-	query.filter.process(req, optionsSchema);
-	query.sorter.process(req, optionsSchema);
-	req.sorter = {
-		id: -1
-	};
-	query.search.process(req, optionsSchema);
-	query.pager.process(req);
-	if (req.body && Object.prototype.hasOwnProperty.call(req.body, 'active')) {
-		if ((req.body.active !== true) && (req.body.active !== false)) {
-			req.body.active = false;
-		}
-	}
+    let optionsSchema = this.getModelSchema(MODEL_NAME);
+    query.filter.process(req, optionsSchema);
+    query.sorter.process(req, optionsSchema);
+    req.sorter = {
+        id: -1,
+    };
+    query.search.process(req, optionsSchema);
+    query.pager.process(req);
+    if (req.body && Object.prototype.hasOwnProperty.call(req.body, "active")) {
+        if (req.body.active !== true && req.body.active !== false) {
+            req.body.active = false;
+        }
+    }
 };
 
 exports.after = () => {};
 
 exports._get = async (req, res) => {
-	log.debug('root options/get');
-	let targetId = req.params._id,
-		userId = req.user._id;
-	try {
-		const notApp = notNode.Application;
-		let
-			thisModel = notApp.getModel(MODEL_NAME);
-		thisModel.getOne(targetId).then((data) => {
-				res.status(200).json({
-					status: 'ok',
-					result: data
-				});
-			})
-			.catch((e) => {
-				log.error(e);
-				notNode.Application.report(new notError('options._get(db)', {
-					ip: getIP(req),
-					userId,
-					targetId
-				}, e));
-				res.status(500).json({
-					status: 'error',
-					error: e.toString()
-				});
-			});
-	} catch (e) {
-		log.error(e);
-		notNode.Application.report(new notError('options._get', {
-			ip: getIP(req),
-			userId,
-			targetId
-		}, e));
-		res.status(500).json({
-			status: 'error',
-			error: e.toString()
-		});
-	}
+    log.debug("root options/get");
+    let targetId = req.params._id,
+        userId = req.user._id;
+    try {
+        const notApp = notNode.Application;
+        let thisModel = notApp.getModel(MODEL_NAME);
+        thisModel
+            .getOne(targetId)
+            .then((data) => {
+                res.status(200).json({
+                    status: "ok",
+                    result: data,
+                });
+            })
+            .catch((e) => {
+                log.error(e);
+                notNode.Application.report(
+                    new notError(
+                        "options._get(db)",
+                        {
+                            ip: getIP(req),
+                            userId,
+                            targetId,
+                        },
+                        e
+                    )
+                );
+                res.status(500).json({
+                    status: "error",
+                    error: e.toString(),
+                });
+            });
+    } catch (e) {
+        log.error(e);
+        notNode.Application.report(
+            new notError(
+                "options._get",
+                {
+                    ip: getIP(req),
+                    userId,
+                    targetId,
+                },
+                e
+            )
+        );
+        res.status(500).json({
+            status: "error",
+            error: e.toString(),
+        });
+    }
 };
 
-
 exports._delete = async (req, res) => {
-	let targetId = req.params._id,
-		userId = req.user._id;
-	try {
-		const notApp = notNode.Application;
-		let thisModel = notApp.getModel(MODEL_NAME);
+    let targetId = req.params._id,
+        userId = req.user._id;
+    try {
+        const notApp = notNode.Application;
+        let thisModel = notApp.getModel(MODEL_NAME);
 
-		let item = await thisModel.findOne({
-			_id: targetId,
-			__latest: true,
-			__closed: false
-		}).exec();
+        let item = await thisModel
+            .findOne({
+                _id: targetId,
+                __latest: true,
+                __closed: false,
+            })
+            .exec();
 
-		if (!item) {
-			return res.status(200).json({
-				status: 'ok'
-			});
-		}
-		notApp.logger.log({
-			module: 'options',
-			model: MODEL_NAME,
-			action: 'delete',
-			by: userId,
-			target: targetId
-		});
-		await item.updateOne({
-			__closed: true
-		}).exec();
-		return res.status(200).json({
-			status: 'ok'
-		});
-	} catch (e) {
-		notNode.Application.report(new notError('options._delete', {
-			ip: getIP(req),
-			userId,
-			targetId
-		}, e));
-		res.status(500).json({
-			status: 'error',
-			error: e.toString()
-		});
-	}
+        if (!item) {
+            return res.status(200).json({
+                status: "ok",
+            });
+        }
+        notApp.logger.log({
+            module: "options",
+            model: MODEL_NAME,
+            action: "delete",
+            by: userId,
+            target: targetId,
+        });
+        await item
+            .updateOne({
+                __closed: true,
+            })
+            .exec();
+        return res.status(200).json({
+            status: "ok",
+        });
+    } catch (e) {
+        notNode.Application.report(
+            new notError(
+                "options._delete",
+                {
+                    ip: getIP(req),
+                    userId,
+                    targetId,
+                },
+                e
+            )
+        );
+        res.status(500).json({
+            status: "error",
+            error: e.toString(),
+        });
+    }
 };
 
 async function updateDocument(doc) {
-	const notApp = notNode.Application;
-	let thisModel = notApp.getModel(MODEL_NAME);
+    const notApp = notNode.Application;
+    let thisModel = notApp.getModel(MODEL_NAME);
 
-	let item = await thisModel.findOne({
-		_id: doc._id,
-		__latest: true,
-		__closed: false
-	}).exec();
+    let item = await thisModel
+        .findOne({
+            _id: doc._id,
+            __latest: true,
+            __closed: false,
+        })
+        .exec();
 
-	if (!item) {
-		throw new notError(notLocale.say('document_not_found'), {
-			doc
-		});
-	}
+    if (!item) {
+        throw new notError(notLocale.say("document_not_found"), {
+            doc,
+        });
+    }
 
-	let updated = await thisModel.findOneAndUpdate({
-		_id: doc._id
-	}, {
-		value: doc.value,
-		active: !!doc.active
-	}, {
-		new: true
-	}).exec();
-	return updated;
+    let updated = await thisModel
+        .findOneAndUpdate(
+            {
+                _id: doc._id,
+            },
+            {
+                value: doc.value,
+                active: !!doc.active,
+            },
+            {
+                new: true,
+            }
+        )
+        .exec();
+    return updated;
 }
 
 exports._update = async (req, res) => {
-	let targetId = req.params._id,
-		_id = req.body._id,
-		userId = req.user._id;
-	try {
-		const notApp = notNode.Application;
-		let thisModel = notApp.getModel(MODEL_NAME);
+    let targetId = req.params._id,
+        _id = req.body._id,
+        userId = req.user._id;
+    try {
+        const notApp = notNode.Application;
+        let thisModel = notApp.getModel(MODEL_NAME);
 
-		let item = await thisModel.findOne({
-			_id: targetId,
-			__latest: true,
-			__closed: false
-		}).exec();
+        let item = await thisModel
+            .findOne({
+                _id: targetId,
+                __latest: true,
+                __closed: false,
+            })
+            .exec();
 
-		if (!item) {
-			return res.status(200).json({
-				status: 'error',
-				error: notLocale.say('document_not_found')
-			});
-		}
-		let data = {
-			value: req.body.value,
-			active: !!req.body.active,
-		};
+        if (!item) {
+            return res.status(200).json({
+                status: "error",
+                error: notLocale.say("document_not_found"),
+            });
+        }
+        let data = {
+            value: req.body.value,
+            active: !!req.body.active,
+        };
 
-		let updated = await thisModel.findOneAndUpdate({
-			_id
-		}, data, {
-			new: true
-		}).exec();
+        let updated = await thisModel
+            .findOneAndUpdate(
+                {
+                    _id,
+                },
+                data,
+                {
+                    new: true,
+                }
+            )
+            .exec();
 
-		return res.status(200).json({
-			status: 'ok',
-			result: updated
-		});
-	} catch (e) {
-		notNode.Application.report(new notError('options._update', {
-			ip: getIP(req),
-			userId,
-			targetId
-		}, e));
-		res.status(500).json({
-			status: 'error',
-			error: e.toString()
-		});
-	}
+        return res.status(200).json({
+            status: "ok",
+            result: updated,
+        });
+    } catch (e) {
+        notNode.Application.report(
+            new notError(
+                "options._update",
+                {
+                    ip: getIP(req),
+                    userId,
+                    targetId,
+                },
+                e
+            )
+        );
+        res.status(500).json({
+            status: "error",
+            error: e.toString(),
+        });
+    }
 };
 
 exports._getForModule = async (req, res) => {
-	let moduleName = req.params.moduleName,
-		userId = req.user._id;
-	try {
-		if ((!moduleName) || (moduleName.length < 1)) {
-			throw new Error('moduleName is now valid');
-		}
-		const notApp = notNode.Application;
-		let results = await notApp.getModel(MODEL_NAME).readModuleOptions(moduleName);
-		res.status(200).json({
-			status: 'ok',
-			result: results
-		});
-	} catch (e) {
-		notNode.Application.report(new notError('options._getForModule', {
-			ip: getIP(req),
-			userId,
-			moduleName
-		}, e));
-		res.status(500).json({
-			status: 'error',
-			error: e.toString()
-		});
-	}
-}
+    let moduleName = req.params.moduleName,
+        userId = req.user._id;
+    try {
+        if (!moduleName || moduleName.length < 1) {
+            throw new Error("moduleName is now valid");
+        }
+        const notApp = notNode.Application;
+        let results = await notApp
+            .getModel(MODEL_NAME)
+            .readModuleOptions(moduleName);
+        res.status(200).json({
+            status: "ok",
+            result: results,
+        });
+    } catch (e) {
+        notNode.Application.report(
+            new notError(
+                "options._getForModule",
+                {
+                    ip: getIP(req),
+                    userId,
+                    moduleName,
+                },
+                e
+            )
+        );
+        res.status(500).json({
+            status: "error",
+            error: e.toString(),
+        });
+    }
+};
 
 exports._updateForModule = async (req, res) => {
-	let moduleName = req.params.moduleName,
-		options = req.body.options,
-		userId = req.user._id;
-	try {
-		if ((!moduleName) || (moduleName.length < 1)) {
-			throw new Error('moduleName is now valid');
-		}
-		const notApp = notNode.Application;
-		const result = await notApp.getModel(MODEL_NAME).writeModuleOptions(moduleName, options);
-		notNode.Application.emit(`module:${moduleName}:options:updated`);
-		const information  = {
-			recipient:{
-				role: 		'root'
-			},
-			tags: 			['moduleOptionsUpdated'],
-			userId,
-			username: req.user.username,
-			userRole: req.user.role.toString(),
-			updatedAt: result.updatedAt,
-			moduleName
-		};
-		setTimeout(()=>{
-			notNode.Application.inform(information);
-		}, 1000);
-		res.status(200).json({
-			status: 'ok'
-		});
-	} catch (e) {
-		notNode.Application.report(new notError('options._updateForModule', {
-			ip: getIP(req),
-			userId,
-			moduleName
-		}, e));
-		res.status(500).json({
-			status: 'error',
-			error: e.toString()
-		});
-	}
-}
-
+    let moduleName = req.params.moduleName,
+        options = req.body.options,
+        userId = req.user._id;
+    try {
+        if (!moduleName || moduleName.length < 1) {
+            throw new Error("moduleName is now valid");
+        }
+        const notApp = notNode.Application;
+        const result = await notApp
+            .getModel(MODEL_NAME)
+            .writeModuleOptions(moduleName, options);
+        notNode.Application.emit(`module:${moduleName}:options:updated`);
+        const information = {
+            recipient: {
+                role: "root",
+            },
+            tags: ["moduleOptionsUpdated"],
+            userId,
+            username: req.user.username,
+            userRole: req.user.role.toString(),
+            updatedAt: result.updatedAt,
+            moduleName,
+        };
+        setTimeout(() => {
+            notNode.Application.inform(information);
+        }, 1000);
+        res.status(200).json({
+            status: "ok",
+        });
+    } catch (e) {
+        notNode.Application.report(
+            new notError(
+                "options._updateForModule",
+                {
+                    ip: getIP(req),
+                    userId,
+                    moduleName,
+                },
+                e
+            )
+        );
+        res.status(500).json({
+            status: "error",
+            error: e.toString(),
+        });
+    }
+};
 
 function getAppName() {
-	return notNode.Application.getEnv('name');
+    return notNode.Application.getEnv("name");
 }
 
 function getOptionsFilename() {
-	let appname = getAppName();
-	let date = new Date().toISOString().split('.')[0];
-	return `${appname}.options.${date}.json`;
+    let appname = getAppName();
+    let date = new Date().toISOString().split(".")[0];
+    return `${appname}.options.${date}.json`;
 }
-
-
 
 exports._import = async (req, res) => {
-	try {
-		let data = JSON.parse(req.body.options);
-		await notNode.Application.getModel(MODEL_NAME).bulkImport(data, true);
-		res.status(200);
-		const timeout = 5000;
-		res.json({
-			status: 'ok',
-			result: {
-				shutdown: timeout,
-				info: `Сервер перезагрузится через ${timeout}мс, страница обновится автоматически`
-			}
-		});
-		res.end();
-		notNode.Application.shutdown(timeout);
-	} catch (e) {
-		notNode.Application.report(new notError('options._import', {
-			ip: getIP(req)
-		}, e));
-		res.status(500).json({
-			status: 'error',
-			error: e.toString()
-		});
-	}
-}
+    try {
+        let data = JSON.parse(req.body.options);
+        await notNode.Application.getModel(MODEL_NAME).bulkImport(data, true);
+        res.status(200);
+        const timeout = 5000;
+        res.json({
+            status: "ok",
+            result: {
+                shutdown: timeout,
+                info: `Сервер перезагрузится через ${timeout}мс, страница обновится автоматически`,
+            },
+        });
+        res.end();
+        notNode.Application.shutdown(timeout);
+    } catch (e) {
+        notNode.Application.report(
+            new notError(
+                "options._import",
+                {
+                    ip: getIP(req),
+                },
+                e
+            )
+        );
+        res.status(500).json({
+            status: "error",
+            error: e.toString(),
+        });
+    }
+};
 
 exports._export = async (req, res) => {
-	try {
-		//taking only last versions of documents
-		let data = await notNode.Application.getModel(MODEL_NAME).bulkExport();
-		let fname = getOptionsFilename();
-		res.status(200);
-		res.append('Content-Disposition', `inline; filename="${fname}"`);
-		res.json(data);
-		res.end();
-	} catch (e) {
-		notNode.Application.report(new notError('options._export', {
-			ip: getIP(req)
-		}, e));
-		res.status(500).json({
-			status: 'error',
-			error: e.toString()
-		});
-	}
-}
+    try {
+        //taking only last versions of documents
+        let data = await notNode.Application.getModel(MODEL_NAME).bulkExport();
+        let fname = getOptionsFilename();
+        res.status(200);
+        res.append("Content-Disposition", `inline; filename="${fname}"`);
+        res.json(data);
+        res.end();
+    } catch (e) {
+        notNode.Application.report(
+            new notError(
+                "options._export",
+                {
+                    ip: getIP(req),
+                },
+                e
+            )
+        );
+        res.status(500).json({
+            status: "error",
+            error: e.toString(),
+        });
+    }
+};
 
 //we have only Admin level routes so, all goes with '_' prefix standart for him
-metaExtend(metaRoute, module.exports, AdminActions, MODEL_OPTIONS, '_');
-metaExtend(metaRoute, module.exports, UserActions, MODEL_OPTIONS, '');
+metaExtend(metaRoute, module.exports, AdminActions, MODEL_OPTIONS, "_");
+metaExtend(metaRoute, module.exports, UserActions, MODEL_OPTIONS, "");
